d9009cea9f82745ac2b9c0e2f700241f342a2c64
jarad <coolgj@163.com>
2023-08-31 19:19:14 +0800

    initial commit

--- connectors/tencent-db-mssql-connector/src/main/java/io/tapdata/connector/mssql/TencentDBMssqlConnector.java
+++
@@ -1,7 +0,0 @@
-package io.tapdata.connector.mssql;
-
-import io.tapdata.pdk.apis.annotations.TapConnectorClass;
-
-@TapConnectorClass("tencent-db-mssql-spec.json")
-public class TencentDBMssqlConnector extends MssqlConnector{
-}
