6cf43125e7bc11cd74809d0acd3d6a5c0cbcb224
jarad <coolgj@163.com>
2023-08-31 19:01:36 +0800

    initial commit

--- connectors/aliyun-rds-mssql-connector/src/main/java/io/tapdata/connector/mssql/AliyunRDSMssqlConnector.java
+++
@@ -1,7 +0,0 @@
-package io.tapdata.connector.mssql;
-
-import io.tapdata.pdk.apis.annotations.TapConnectorClass;
-
-@TapConnectorClass("aliyun-rds-mssql-spec.json")
-public class AliyunRDSMssqlConnector extends MssqlConnector{
-}
