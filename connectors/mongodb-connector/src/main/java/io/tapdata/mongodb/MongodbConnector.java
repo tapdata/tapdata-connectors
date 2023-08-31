62778ec80c3927da3ca3c8a846f666c657455cb6
jarad <coolgj@163.com>
2023-08-31 19:14:14 +0800

    initial commit

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
@@ -801,7 +801,7 @@ public class MongodbConnector extends ConnectorBase {
 		return value;
 	}
 
-	private void getReadPartitions(TapConnectorContext connectorContext, TapTable table, GetReadPartitionOptions options) throws Throwable {
+	private void getReadPartitions(TapConnectorContext connectorContext, TapTable table, GetReadPartitionOptions options) {
 		options.getTypeSplitterMap().registerCustomSplitter(ObjectId.class, new ObjectIdSplitter());
 
 		DatabaseReadPartitionSplitter.calculateDatabaseReadPartitions(connectorContext, table, options)
