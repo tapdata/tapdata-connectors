d8bed9b82638e8e581675a49de031d502aa67692
jarad <coolgj@163.com>
2023-09-04 23:13:33 +0800

    fix: TDEngine writeRecord with ``

--- connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/TDengineWriteRecorder.java
+++ connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/TDengineWriteRecorder.java
@@ -2,21 +2,15 @@ package io.tapdata.connector.tdengine;
 
 import io.tapdata.common.WriteRecorder;
 import io.tapdata.entity.event.dml.TapRecordEvent;
-import io.tapdata.entity.logger.TapLogger;
 import io.tapdata.entity.schema.TapTable;
-import io.tapdata.entity.schema.value.DateTime;
 import io.tapdata.kit.EmptyKit;
 import io.tapdata.kit.StringKit;
-import io.tapdata.pdk.apis.entity.ConnectionOptions;
 import io.tapdata.pdk.apis.entity.WriteListResult;
-import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class TDengineWriteRecorder extends WriteRecorder {
@@ -40,8 +34,8 @@ public class TDengineWriteRecorder extends WriteRecorder {
     //just insert
     private void justInsert(Map<String, Object> after) throws SQLException {
         if (EmptyKit.isNull(preparedStatement)) {
-            String insertSql = "INSERT INTO \"" + schema + "\".\"" + tapTable.getId() + "\" ("
-                    + allColumn.stream().map(k -> "\"" + k + "\"").collect(Collectors.joining(", ")) + ") " +
+            String insertSql = "INSERT INTO `" + schema + "`.`" + tapTable.getId() + "` ("
+                    + allColumn.stream().map(k -> "`" + k + "`").collect(Collectors.joining(", ")) + ") " +
                     "VALUES(" + StringKit.copyString("?", allColumn.size(), ",") + ") ";
             preparedStatement = connection.prepareStatement(insertSql);
         }
@@ -67,8 +61,7 @@ public class TDengineWriteRecorder extends WriteRecorder {
         }
         if (EmptyKit.isNull(preparedStatement)) {
             if (EmptyKit.isNotBlank(timestampField)) {
-
-                preparedStatement = connection.prepareStatement(String.format("DELETE FROM %s.%s WHERE %s='%s'",
+                preparedStatement = connection.prepareStatement(String.format("DELETE FROM `%s`.`%s` WHERE `%s`='%s'",
                         schema, tapTable.getId(), timestampField, before.get(timestampField)));
             }
         }
