848d77ea140fbbeeeeb1622d3003936ad4966a74
ljvv7 <fes504200361@qq.com>
2023-12-04 10:24:57 +0800

    feature(CosmosDB):CosmosDB batchRead

---
+++ connectors/azure-cosmosdb-connector/src/main/java/io/tapdata/connector/azure/cosmosdb/util/NumberNodeType.java
@@ -0,0 +1,14 @@
+package io.tapdata.connector.azure.cosmosdb.util;
+
+public enum NumberNodeType {
+    DOUBLE(),
+    DECIMAL(),
+    FLOAT(),
+    INT(),
+    LONG(),
+    BIGINTEGER(),
+    SHORT();
+
+
+
+}
