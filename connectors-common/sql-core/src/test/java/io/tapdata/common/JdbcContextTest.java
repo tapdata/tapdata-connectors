83840fef49aff6044922adf058054f1886f66c89
GavinXiao <2749984520@qq.com>
2024-03-08 19:22:47 +0800

    fix(sql-core): remove useless code

--- connectors-common/sql-core/src/test/java/io/tapdata/common/JdbcContextTest.java
+++ connectors-common/sql-core/src/test/java/io/tapdata/common/JdbcContextTest.java
@@ -1,74 +1,12 @@
 package io.tapdata.common;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-
-import java.util.Properties;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
-
 public class JdbcContextTest {
 
     @Nested
     class HikariConnectionTest {
-//
-//        @Nested
-//        class GetHikariDataSourceTest {
-//            CommonDbConfig config;
-//            @BeforeEach
-//            void init() {
-//                config = mock(CommonDbConfig.class);
-//
-//                when(config.getJdbcDriver()).thenReturn("");
-//                when(config.getDatabaseUrl()).thenReturn("");
-//                when(config.getUser()).thenReturn("");
-//                when(config.getPassword()).thenReturn("");
-//                when(config.getProperties()).thenReturn(mock(Properties.class));
-//            }
-//            void assertVerify(CommonDbConfig c, int jd, int db, int u, int pwd, int pro) {
-//                try (MockedStatic<JdbcContext.HikariConnection> jk = mockStatic(JdbcContext.HikariConnection.class)){
-//                    jk.when(() -> JdbcContext.HikariConnection.getHikariDataSource(c)).thenCallRealMethod();
-//
-//                    HikariDataSource source = JdbcContext.HikariConnection.getHikariDataSource(c);
-//                    Assertions.assertNotNull(source);
-//
-//                    verify(config, times(jd)).getJdbcDriver();
-//                    verify(config, times(db)).getDatabaseUrl();
-//                    verify(config, times(u)).getUser();
-//                    verify(config, times(pwd)).getPassword();
-//                    verify(config, times(pro)).getProperties();
-//                }
-//            }
-//
-//            @Test
-//            void testCommonDbConfigIsNull() {
-//                Assertions.assertThrows(IllegalArgumentException.class, () -> {
-//                    assertVerify(null , 0, 0, 0, 0, 0);
-//                });
-//            }
-//            @Test
-//            void testPropertiesIsNull() {
-//                when(config.getProperties()).thenReturn(null);
-//                Assertions.assertDoesNotThrow(() -> {
-//                    assertVerify(null , 1, 1, 1, 1, 1);
-//                });
-//            }
-//            @Test
-//            void testNormal() {
-//                Assertions.assertDoesNotThrow(() -> {
-//                    assertVerify(config , 1, 1, 1, 1, 2);
-//                });
-//            }
-//        }
-
         @Nested
         class GetParamFromUrlTest {
             @Test
