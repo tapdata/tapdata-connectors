b7f5d240a144a925322becedcdca696887839a8c
hardy <2545433047@qq.com>
2024-03-25 14:21:18 +0800

    feat:add aws_clickhouse connector

--- connectors/aws-clickhouse-connector/src/main/java/io/tapdata/connector/aws/clickhouse/AwsClickhouseConnector.java
+++ connectors/aws-clickhouse-connector/src/main/java/io/tapdata/connector/aws/clickhouse/AwsClickhouseConnector.java
@@ -2,10 +2,15 @@ package io.tapdata.connector.aws.clickhouse;
 
 import io.tapdata.connector.aws.clickhouse.config.AwsClickhouseConfig;
 import io.tapdata.connector.clickhouse.ClickhouseConnector;
+import io.tapdata.connector.clickhouse.config.ClickhouseConfig;
 import io.tapdata.pdk.apis.annotations.TapConnectorClass;
 import io.tapdata.pdk.apis.context.TapConnectionContext;
+import io.tapdata.pdk.apis.entity.ConnectionOptions;
+import io.tapdata.pdk.apis.entity.TestItem;
 
 import java.sql.SQLException;
+import java.util.function.Consumer;
+
 @TapConnectorClass("aws_clickhouse.json")
 public class AwsClickhouseConnector extends ClickhouseConnector {
 
@@ -14,5 +19,16 @@ public class AwsClickhouseConnector extends ClickhouseConnector {
     protected void initConnection(TapConnectionContext connectionContext) throws SQLException {
         super.initConnection(connectionContext);
         clickhouseConfig = new AwsClickhouseConfig().load(connectionContext.getConnectionConfig());
+    }
+    @Override
+    public ConnectionOptions connectionTest(TapConnectionContext connectionContext, Consumer<TestItem> consumer) {
+        ConnectionOptions connectionOptions = ConnectionOptions.create();
+        clickhouseConfig = new ClickhouseConfig().load(connectionContext.getConnectionConfig());
+        try (
+                AwsClickhouseTest clickhouseTest = new AwsClickhouseTest(clickhouseConfig, consumer)
+        ) {
+            clickhouseTest.testOneByOne();
+            return connectionOptions;
+        }
     }
 }
