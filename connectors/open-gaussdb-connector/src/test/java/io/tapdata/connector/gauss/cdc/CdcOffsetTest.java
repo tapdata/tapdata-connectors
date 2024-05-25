a303579a9c1a86acd0ac7bccdc49f2326af19cc3
GavinXiao <2749984520@qq.com>
2024-05-25 14:51:31 +0800

    test(gaussdb): add unit test case

--- connectors/open-gaussdb-connector/src/test/java/io/tapdata/connector/gauss/cdc/CdcOffsetTest.java
+++ connectors/open-gaussdb-connector/src/test/java/io/tapdata/connector/gauss/cdc/CdcOffsetTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doCallRealMethod;
@@ -61,5 +63,61 @@ public class CdcOffsetTest {
     void testSetTransactionTimestamp() {
         doCallRealMethod().when(offset).setTransactionTimestamp(anyLong());
         Assertions.assertDoesNotThrow(() -> offset.setTransactionTimestamp(10L));
+    }
+
+    @Test
+    void testMap() {
+        CdcOffset offset = CdcOffset.fromOffset(new HashMap<>());
+        Assertions.assertEquals(0L, offset.getXidIndex());
+        Assertions.assertEquals(0, offset.getXidIndex());
+    }
+
+    @Test
+    void testMap1() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.XID_INDEX, 11);
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(11, offset.getXidIndex());
+    }
+    @Test
+    void testMap11() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.XID_INDEX, "");
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(0, offset.getXidIndex());
+    }
+
+    @Test
+    void testMap2() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.LSN, 11L);
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(0, offset.getXidIndex());
+        Assertions.assertEquals(11L, offset.getLsn());
+        Assertions.assertEquals(0, offset.getXidIndex());
+    }
+    @Test
+    void testMap21() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.LSN, "11L");
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(0, offset.getXidIndex());
+        Assertions.assertNull(offset.getLsn());
+    }
+    @Test
+    void testMap3() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.TRANSACTION_TIMESTAMP, 11L);
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(0, offset.getXidIndex());
+        Assertions.assertEquals(0L, offset.getXidIndex());
+    }
+    @Test
+    void testMap31() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(CdcOffset.TRANSACTION_TIMESTAMP, "11L");
+        CdcOffset offset = CdcOffset.fromOffset(map);
+        Assertions.assertEquals(0, offset.getXidIndex());
+        Assertions.assertEquals(0L, offset.getXidIndex());
     }
 }
