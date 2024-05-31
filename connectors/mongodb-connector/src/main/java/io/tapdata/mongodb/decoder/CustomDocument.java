2a27ce7e8484cbe702b7a2bd7396a1c1da607593
sam.p <102567930@qq.com>
2024-05-31 18:30:24 +0800

    fix(mongodb): 时间字段做关联键的聚合任务，反查不到数据
    Closes TAP-2920

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/CustomDocument.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/CustomDocument.java
@@ -1,5 +1,6 @@
 package io.tapdata.mongodb.decoder;
 
+import io.tapdata.mongodb.decoder.impl.DateTimeReader;
 import io.tapdata.mongodb.decoder.impl.DateToTimestamp;
 import io.tapdata.mongodb.decoder.impl.DynamicDateFilterTime;
 import org.bson.Document;
@@ -16,7 +17,7 @@ public class CustomDocument {
         return Document.parse(json, new CustomDocumentDecoder()
                 .registerCustomReader(DynamicDateFilterTime.DYNAMIC_DATE, originalValue -> new DynamicDateFilterTime().execute(originalValue, null))
                 .registerCustomReader(DateToTimestamp.DATE_TO_TIMESTAMP, originalValue -> new DateToTimestamp().execute(originalValue, null))
+                .registerCustomReader(DateTimeReader.DATE_TIME_KEY, originalValue -> new DateTimeReader().execute(originalValue, null))
         );
-
     }
 }
