fa986517be0790bd411202cbdffa4eeb10464667
sam.p <102567930@qq.com>
2024-05-31 18:30:24 +0800

    fix(mongodb): 时间字段做关联键的聚合任务，反查不到数据
    Closes TAP-2920

--- connectors/mongodb-connector/src/test/java/io/tapdata/mongodb/decoder/CustomDocumentTest.java
+++ connectors/mongodb-connector/src/test/java/io/tapdata/mongodb/decoder/CustomDocumentTest.java
@@ -1,11 +1,20 @@
 package io.tapdata.mongodb.decoder;
 
+import io.tapdata.entity.schema.value.DateTime;
+import io.tapdata.entity.simplify.TapSimplify;
+import io.tapdata.entity.utils.JsonParser;
 import org.bson.Document;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomDocumentTest {
 
@@ -15,26 +24,44 @@ public class CustomDocumentTest {
         @Test
         void testParse() {
             Document parse = CustomDocument.parse("");
-            Assertions.assertNotNull(parse);
+            assertNotNull(parse);
         }
         @Test
         void testNullJsonParse() {
             Document parse = CustomDocument.parse(null);
-            Assertions.assertNotNull(parse);
+            assertNotNull(parse);
         }
         @Test
         void testNormalJsonParse() {
             Document parse = CustomDocument.parse("{\"create\":{\"$dynamicDate\":{\"format\":\"2024-03-07\"}}}");
-            Assertions.assertNotNull(parse);
-            Assertions.assertNotNull(parse.get("create"));
+            assertNotNull(parse);
+            assertNotNull(parse.get("create"));
             Assertions.assertEquals(Date.class.getName(), parse.get("create").getClass().getName());
         }
         @Test
         void testJsonParseCovertToTimestamp(){
             Document parse = CustomDocument.parse("{\"create\":{\"$dateToTimestamp\": {\"$dynamicDate\":{\"format\":\"2024-03-07\"}}}}");
-            Assertions.assertNotNull(parse);
-            Assertions.assertNotNull(parse.get("create"));
+            assertNotNull(parse);
+            assertNotNull(parse.get("create"));
             Assertions.assertEquals(Long.class.getName(), parse.get("create").getClass().getName());
+        }
+
+        @Test
+        @DisplayName("test DateTimeReader")
+        void testDateTime() {
+            Document parse = CustomDocument.parse("{\"valDate\":{\"$dateTime\":{\"fraction\":3,\"nano\":438000000,\"originType\":90,\"seconds\":1717151086,\"timeZone\":null}}}");
+            assertNotNull(parse);
+            assertNotNull(parse.get("valDate"));
+            Object valDate = parse.get("valDate");
+            assertInstanceOf(Instant.class, valDate);
+            assertEquals(1717151086, ((Instant) valDate).getEpochSecond());
+            assertEquals(438000000, ((Instant) valDate).getNano());
+
+            Document parse1 = CustomDocument.parse("{\"valDate\": {\"$dateTime\": \"test\"}}");
+            assertNotNull(parse1);
+            assertNotNull(parse1.get("valDate"));
+            assertInstanceOf(String.class, parse1.get("valDate"));
+            assertEquals("test", parse1.get("valDate"));
         }
     }
 }
