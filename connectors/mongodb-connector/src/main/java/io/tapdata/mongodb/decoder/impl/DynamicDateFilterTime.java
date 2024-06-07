fca874c2806f9432f5487935ebe5d173aaabf523
GavinXiao <2749984520@qq.com>
2024-06-07 19:33:15 +0800

    feat(mongo): add timezone

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/impl/DynamicDateFilterTime.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/impl/DynamicDateFilterTime.java
@@ -4,25 +4,30 @@ import io.tapdata.entity.error.CoreException;
 import io.tapdata.mongodb.decoder.CustomSQLObject;
 
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TimeZone;
 
 public class DynamicDateFilterTime implements CustomSQLObject<Object, Map<String, Object>> {
     public static final String DYNAMIC_DATE = "$dynamicDate";
     public static final String CUSTOM_FORMAT = "customFormat";
     public static final String TO_STRING_FORMAT = "toStringFormat";
     public static final String SUBTRACT = "subtract";
+    public static final String TIMEZONE = "timezone";
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
 
     @Override
     public Object execute(Object functionObj, Map<String, Object> curMap) {
         Object format = null;
         Object toStringFormat = null;
+        Object timezone = null;
         int subtract = 0;
         if (functionObj instanceof Map) {
             format = ((Map<String, Object>) functionObj).get(CUSTOM_FORMAT);
+            timezone = ((Map<String, Object>) functionObj).get(TIMEZONE);
             toStringFormat = ((Map<String, Object>) functionObj).get(TO_STRING_FORMAT);
             Object sub = Optional.ofNullable(((Map<String, Object>) functionObj).get(SUBTRACT)).orElse(0);
             if (sub instanceof Number) {
@@ -38,10 +43,18 @@ public class DynamicDateFilterTime implements CustomSQLObject<Object, Map<String
         if (null != format && !(format instanceof String)) {
             throw new IllegalArgumentException("a customFormat value is illegal value, \"" + CUSTOM_FORMAT + "\" must be a date time format string, such as: \"yyyy-MM-dd HH:mm:ss.SSS\"");
         }
+        TimeZone timeZone = null;
+        if (null != timezone) {
+            try {
+                timeZone = TimeZone.getTimeZone(ZoneId.of(String.valueOf(timezone)));
+            } catch (Exception e) {
+                throw new IllegalArgumentException("a timezone value is illegal value, \"" + TIMEZONE + "\" must be a Number, such as: 8 or -8");
+            }
+        }
         if (null != toStringFormat && !(toStringFormat instanceof String)) {
             throw new IllegalArgumentException("a toStringFormat value is illegal value, \"" + TO_STRING_FORMAT + "\" must be a date time format string, such as: \"yyyy-MM-dd HH:mm:ss\"");
         }
-        return covertTime((String) format, subtract, (String) toStringFormat);
+        return covertTime((String) format, subtract, (String) toStringFormat, timeZone);
     }
 
     @Override
@@ -49,13 +62,16 @@ public class DynamicDateFilterTime implements CustomSQLObject<Object, Map<String
         return DYNAMIC_DATE;
     }
 
-    protected Object covertTime(String dateTime, int subtract, String toStringFormat) {
+    protected Object covertTime(String dateTime, int subtract, String toStringFormat, TimeZone timeZone) {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.MILLISECOND, -subtract);
         Date formatDate = calendar.getTime();
         if (null != dateTime) {
             try {
                 SimpleDateFormat sdf = new SimpleDateFormat(dateTime);
+                if (null != timeZone) {
+                    sdf.setTimeZone(timeZone);
+                }
                 String dateFormatStr = sdf.format(formatDate);
                 sdf.applyPattern(DEFAULT_DATE_FORMAT);
                 formatDate = sdf.parse(dateFormatStr);
@@ -68,6 +84,9 @@ public class DynamicDateFilterTime implements CustomSQLObject<Object, Map<String
         if (null != toStringFormat) {
             try {
                 SimpleDateFormat sdf = new SimpleDateFormat(toStringFormat);
+                if (null != timeZone) {
+                    sdf.setTimeZone(timeZone);
+                }
                 return sdf.format(formatDate);
             } catch (Exception e) {
                 throw new CoreException("Illegal argument in function: {}, {}'s wrong value: {}, error message: {}",
