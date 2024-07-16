07d178eaa1ebfc011d28d9118b405689eeab3198
jarad0628 <coolgj@163.com>
2024-07-17 01:18:59 +0800

    fix: write time with timezone

--- connectors/mysql-connector/src/main/java/io/tapdata/connector/mysql/MysqlConnector.java
+++ connectors/mysql-connector/src/main/java/io/tapdata/connector/mysql/MysqlConnector.java
@@ -495,7 +495,7 @@ public class MysqlConnector extends CommonDbConnector {
                         if (mysqlConfig.getOldVersionTimezone()) {
                             data.put(columnName, resultSet.getString(i + 1));
                         } else if (value instanceof java.sql.Date) {
-                            data.put(columnName, ((java.sql.Date) value).toLocalDate());
+                            data.put(columnName, ((java.sql.Date) value).toLocalDate().atStartOfDay());
                         } else {
                             data.put(columnName, value);
                         }
