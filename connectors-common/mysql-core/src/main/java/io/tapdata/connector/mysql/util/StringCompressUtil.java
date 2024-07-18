5b5280de92c1d7a62548826d1b4cfaf1e60befd6
Gavin'Xiao <2749984520@qq.com>
2024-07-18 18:30:04 +0800

    fix(TAP-3946): inappropriate handling of table model operations leads to OOM from mysql

--- connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/util/StringCompressUtil.java
+++ connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/util/StringCompressUtil.java
@@ -12,31 +12,34 @@ import java.util.zip.GZIPOutputStream;
  * @create 2022-05-25 17:41
  **/
 public class StringCompressUtil {
-	private static final String DEFAULT_CODE = "ISO-8859-1";
+    private static final String DEFAULT_CODE = "ISO-8859-1";
 
-	public static String compress(String str) throws IOException {
-		if (str == null || str.length() == 0) {
-			return str;
-		}
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		GZIPOutputStream gzip = new GZIPOutputStream(out);
-		gzip.write(str.getBytes());
-		gzip.close();
-		return out.toString(DEFAULT_CODE);
-	}
+    public static String compress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             GZIPOutputStream gzip = new GZIPOutputStream(out)) {
+            gzip.write(str.getBytes());
+            String string = out.toString(DEFAULT_CODE);
+            return string;
+        }
+    }
 
-	public static String uncompress(String str) throws IOException {
-		if (str == null || str.length() == 0) {
-			return str;
-		}
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		ByteArrayInputStream in = new ByteArrayInputStream(str.getBytes(DEFAULT_CODE));
-		GZIPInputStream gunzip = new GZIPInputStream(in);
-		byte[] buffer = new byte[256];
-		int n;
-		while ((n = gunzip.read(buffer)) >= 0) {
-			out.write(buffer, 0, n);
-		}
-		return out.toString(DEFAULT_CODE);
-	}
+    public static String uncompress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             ByteArrayInputStream in = new ByteArrayInputStream(str.getBytes(DEFAULT_CODE));
+             GZIPInputStream gunzip = new GZIPInputStream(in)) {
+            byte[] buffer = new byte[256];
+            int n;
+            while ((n = gunzip.read(buffer)) >= 0) {
+                out.write(buffer, 0, n);
+            }
+            String string = out.toString(DEFAULT_CODE);
+            return string;
+        }
+    }
 }
