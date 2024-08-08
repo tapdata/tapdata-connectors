5b5d8f4a0a1e0faa17a7c468669dfefcc9cfeec8
sam.p <102567930@qq.com>
2024-08-08 18:33:38 +0800

    fix(custom): TAP-4276 Data Pipeline Issue: Batch Read Number Set to 1 Only Reads One Record Instead of all records when source node is custom connection
    Closes TAP-4276

--- connectors/custom-connector/src/test/java/io/tapdata/connector/custom/CustomConnectorTest.java
+++ connectors/custom-connector/src/test/java/io/tapdata/connector/custom/CustomConnectorTest.java
@@ -1,21 +1,30 @@
 package io.tapdata.connector.custom;
 
-import com.hazelcast.core.HazelcastInstance;
-import com.tapdata.constant.HazelcastUtil;
 import io.tapdata.connector.custom.config.CustomConfig;
 import io.tapdata.entity.event.TapEvent;
 import io.tapdata.entity.logger.Log;
 import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.script.ScriptFactory;
+import io.tapdata.entity.utils.BeanUtils;
+import io.tapdata.entity.utils.InstanceFactory;
+import io.tapdata.entity.utils.ObjectSerializable;
+import io.tapdata.entity.utils.TapUtils;
 import io.tapdata.pdk.apis.context.TapConnectorContext;
+import io.tapdata.pdk.apis.utils.TypeConverter;
+import io.tapdata.pdk.core.api.impl.serialize.ObjectSerializableImplV2;
+import io.tapdata.script.factory.TapdataScriptFactory;
 import io.tapdata.script.factory.script.TapRunScriptEngine;
 import org.junit.jupiter.api.*;
 import org.mockito.MockedStatic;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 /**
@@ -25,12 +34,25 @@ import static org.mockito.Mockito.*;
  **/
 class CustomConnectorTest {
 
+	private MockedStatic<InstanceFactory> instanceFactoryMockedStatic;
 	private CustomConnector customConnector;
 
 	@BeforeEach
 	void setUp() {
+		instanceFactoryMockedStatic = mockStatic(InstanceFactory.class);
+		TapdataScriptFactory tapdataScriptFactory = new TapdataScriptFactory();
+		instanceFactoryMockedStatic.when(() -> InstanceFactory.instance(ScriptFactory.class, "engine")).thenReturn(tapdataScriptFactory);
+		instanceFactoryMockedStatic.when(() -> InstanceFactory.instance(TypeConverter.class)).thenReturn(mock(TypeConverter.class));
+		instanceFactoryMockedStatic.when(() -> InstanceFactory.instance(TapUtils.class)).thenReturn(mock(TapUtils.class));
+		instanceFactoryMockedStatic.when(() -> InstanceFactory.instance(BeanUtils.class)).thenReturn(mock(BeanUtils.class));
+		instanceFactoryMockedStatic.when(() -> InstanceFactory.instance(ObjectSerializable.class)).thenReturn(new ObjectSerializableImplV2());
 		customConnector = spy(new CustomConnector());
 		doReturn(true).when(customConnector).isAlive();
+	}
+
+	@AfterEach
+	void tearDown() {
+		Optional.ofNullable(instanceFactoryMockedStatic).ifPresent(MockedStatic::close);
 	}
 
 	@Nested
@@ -39,16 +61,12 @@ class CustomConnectorTest {
 
 		private TapConnectorContext tapConnectorContext;
 		private int count;
-		private MockedStatic<HazelcastUtil> hazelcastUtilMockedStatic;
 
 		@BeforeEach
 		void setUp() {
 			tapConnectorContext = mock(TapConnectorContext.class);
 			Log log = mock(Log.class);
 			when(tapConnectorContext.getLog()).thenReturn(log);
-			hazelcastUtilMockedStatic = mockStatic(HazelcastUtil.class);
-			HazelcastInstance hazelcastInstance = mock(HazelcastInstance.class);
-			hazelcastUtilMockedStatic.when(HazelcastUtil::getInstance).thenReturn(hazelcastInstance);
 			CustomConfig customConfig = new CustomConfig();
 			customConfig.setJsEngineName(TapRunScriptEngine.EngineType.GRAALVM_JS.name());
 			count = 123;
@@ -57,11 +75,6 @@ class CustomConnectorTest {
 					"  core.push({\"id\": id, \"title\": \"xxxx\", \"created\": new Date()})  \n" +
 					"}");
 			ReflectionTestUtils.setField(customConnector, "customConfig", customConfig);
-		}
-
-		@AfterEach
-		void tearDown() {
-			Optional.ofNullable(hazelcastUtilMockedStatic).ifPresent(MockedStatic::close);
 		}
 
 		@Test
