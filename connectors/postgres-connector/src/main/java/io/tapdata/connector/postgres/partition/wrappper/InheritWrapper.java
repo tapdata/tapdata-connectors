e53f75f9cc4a6c17024a3496795efadb6d74386b
Gavin'Xiao <2749984520@qq.com>
2024-08-09 16:01:05 +0800

    feat(TAP-3839): pg support partition table

--- connectors/postgres-connector/src/main/java/io/tapdata/connector/postgres/partition/wrappper/InheritWrapper.java
+++ connectors/postgres-connector/src/main/java/io/tapdata/connector/postgres/partition/wrappper/InheritWrapper.java
@@ -2,6 +2,7 @@ package io.tapdata.connector.postgres.partition.wrappper;
 
 import io.tapdata.entity.logger.Log;
 import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.schema.partition.type.TapPartitionStage;
 import io.tapdata.entity.schema.partition.type.TapPartitionType;
 
 import java.util.List;
@@ -9,12 +10,12 @@ import java.util.List;
 public class InheritWrapper extends PGPartitionWrapper {
 
     @Override
-    public TapPartitionType.Type type() {
-        return TapPartitionType.Type.INHERIT;
+    public TapPartitionStage type() {
+        return TapPartitionStage.INHERIT;
     }
 
     @Override
-    public List<? extends TapPartitionType> parse(TapTable table, String partitionSQL, String checkOrPartitionRule, Log log) {
+    public List<TapPartitionType> parse(TapTable table, String partitionSQL, String checkOrPartitionRule, Log log) {
         return null;
     }
 }
