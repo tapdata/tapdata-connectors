a295c3ccffac3ac141dc267e3e72e179abccd5fa
Gavin'Xiao <2749984520@qq.com>
2024-08-21 17:55:58 +0800

    fix(pg): support time with time zone

--- connectors/postgres-connector/src/main/java/io/tapdata/connector/postgres/partition/wrappper/RangeWrapper.java
+++ connectors/postgres-connector/src/main/java/io/tapdata/connector/postgres/partition/wrappper/RangeWrapper.java
@@ -7,8 +7,11 @@ import io.tapdata.entity.schema.partition.type.TapPartitionStage;
 import io.tapdata.entity.schema.partition.type.TapPartitionType;
 import io.tapdata.entity.schema.partition.type.TapRangeValue;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,16 +39,37 @@ public class RangeWrapper extends PGPartitionWrapper {
             for (int index = 0; index < fromValues.length; index++) {
                 String from = String.valueOf(fromValues[index]).trim();
                 String to = String.valueOf(toValues[index]).trim();
-
                 partitionRanges.add(new TapPartitionRange<String>()
-                        .from(new TapRangeValue<>(from, valueType(from.toUpperCase()), from))
-                        .to(new TapRangeValue<>(to, valueType(to.toUpperCase()), to))
+                        .from(new TapRangeValue<>(filter(from, log), valueType(from.toUpperCase()), from))
+                        .to(new TapRangeValue<>(filter(to, log), valueType(to.toUpperCase()), to))
                 );
             }
             return partitionRanges;
         }
         log.warn("Cant not wrapper from and to value to set Range Partition info, partition sql: {}", partitionSQL);
         return null;
+    }
+
+    protected String filter(String value, Log log) {
+        try {
+            Pattern pattern = Pattern.compile("^'\\d{4}-\\d{1,2}-\\d{1,2} \\d{1,2}:\\d{1,2}:\\d{1,2}[+-]\\d{2}'$");
+            Matcher matcher = pattern.matcher(value);
+            if (matcher.find()) {
+                value = value.substring(1, value.length() - 1);
+                String timestamp = value.substring(0, value.length() - 3);
+                String timezone = value.substring(value.length() - 3);
+                String format = (Timestamp.valueOf(timestamp).toLocalDateTime().minusHours(Long.parseLong(timezone)))
+                        .format(new DateTimeFormatterBuilder()
+                                .appendPattern("yyyy-MM-dd HH:mm:ss")
+                                .optionalStart()
+                                .optionalEnd()
+                                .toFormatter());
+                return String.format("'%s'", format);
+            }
+        } catch (Exception e) {
+            log.warn("Cant not wrapper time with time zone value to set Range Partition info, origin time format value: {}", value);
+        }
+        return value;
     }
 
     protected TapRangeValue.ValueType valueType(String value) {
