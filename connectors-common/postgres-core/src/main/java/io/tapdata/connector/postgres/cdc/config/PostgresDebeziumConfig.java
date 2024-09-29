7e83746f3bbac53b005a2a7d923c1b3e34047868
hardy <2545433047@qq.com>
2024-09-29 11:19:41 +0800

    TAP-4917:fix postgres debezium oom

--- connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/cdc/config/PostgresDebeziumConfig.java
+++ connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/cdc/config/PostgresDebeziumConfig.java
@@ -114,7 +114,9 @@ public class PostgresDebeziumConfig {
                 .with("other.type", "io.tapdata.connector.postgres.converters.OtherConverter")
                 .with("other.schema.name", "io.debezium.postgresql.type.Other")
                 .with("heartbeat.interval.ms", 3000)
-                .with("plugin.name", postgresConfig.getLogPluginName());
+                .with("plugin.name", postgresConfig.getLogPluginName())
+                .with("max.queue.size", postgresConfig.getMaximumQueueSize())
+                .with("max.batch.size", postgresConfig.getMaximumQueueSize() / 8);
         if (Boolean.TRUE.equals(postgresConfig.getDoubleActive())) {
             builder.with("provide.transaction.metadata", true);
         }
