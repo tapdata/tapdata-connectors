2370ec8cd02c10eb35925bb7ef2d5c0501567361
jarad0628 <coolgj@163.com>
2024-10-30 11:35:58 +0800

    fix: tdengine as target support update event

--- connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/TDengineConnector.java
+++ connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/TDengineConnector.java
@@ -230,7 +230,7 @@ public class TDengineConnector extends CommonDbConnector {
             new TDengineRecordWriter(tdengineJdbcContext, tapTable, timestamp)
                     .setInsertPolicy(insertDmlPolicy)
                     .setUpdatePolicy(updateDmlPolicy)
-                    .write(tapRecordEvents, consumer);
+                    .write(tapRecordEvents, consumer, this::isAlive);
         }
     }
 
