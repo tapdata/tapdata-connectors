ba27bafec5c6e179826d9a3b1fbc7504fc11aa27
jarad0628 <coolgj@163.com>
2025-01-16 17:44:02 +0800

    fix: geometry(Point, 4326) type bug

--- connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/bean/PostgresColumn.java
+++ connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/bean/PostgresColumn.java
@@ -4,6 +4,7 @@ import io.tapdata.common.CommonColumn;
 import io.tapdata.entity.schema.TapField;
 import io.tapdata.entity.utils.DataMap;
 import io.tapdata.kit.EmptyKit;
+import io.tapdata.kit.StringKit;
 
 /**
  * @author Jarad
@@ -20,6 +21,10 @@ public class PostgresColumn extends CommonColumn {
         this.dataType = dataMap.getString("dataType").replaceAll("\\[]", " array"); //'dataType' with precision and scale (postgres has its function)
 //        this.dataType = dataMap.getString("data_type"); //'data_type' without precision or scale
         this.pureDataType = dataMap.getString("pureDataType");
+        if ("USER-DEFINED".equals(this.pureDataType)) {
+            this.dataType = StringKit.removeParentheses(this.dataType);
+            this.pureDataType = this.dataType;
+        }
         this.nullable = dataMap.getString("nullable");
         this.remarks = dataMap.getString("columnComment");
         //create table in target has no need to set default value
