846d25bae2cbe17f7f82d2857225a2f5851f8a1e
jarad0628 <coolgj@163.com>
2025-01-17 14:57:09 +0800

    fix: idea cherry Pick problem

--- connectors-common/connector-core/src/main/java/io/tapdata/kit/DbKit.java
+++ connectors-common/connector-core/src/main/java/io/tapdata/kit/DbKit.java
@@ -198,9 +198,6 @@ public class DbKit {
             }
             lastAfter.put(entry.getKey(), entry.getValue());
         }
-        if (EmptyKit.isEmpty(lastAfter)) {
-            return after;
-        }
         return lastAfter;
     }
 
@@ -215,6 +212,9 @@ public class DbKit {
                 continue;
             }
             lastAfter.put(entry.getKey(), entry.getValue());
+        }
+        if (EmptyKit.isEmpty(lastAfter)) {
+            return after;
         }
         return lastAfter;
     }
