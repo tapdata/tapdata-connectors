99d568bbc701054152034a7551c672d93ace70cd
Berry <xbsura@gmail.com>
2025-03-31 16:42:33 +0800

    fix:  修复 starrocks 的一些小问题

--- connectors/starrocks-connector/src/main/java/io/tapdata/connector/starrocks/streamload/Constants.java
+++ connectors/starrocks-connector/src/main/java/io/tapdata/connector/starrocks/streamload/Constants.java
@@ -7,7 +7,7 @@ package io.tapdata.connector.starrocks.streamload;
 public class Constants {
     public static final String FIELD_DELIMITER_DEFAULT = "||%%||";
     public static final String LINE_DELIMITER_DEFAULT = "\n";
-    public static final String Starrocks_DELETE_SIGN = "__STARROCKS_DELETE_SIGN__";
+    public static final String Starrocks_DELETE_SIGN = "__op";
     public static final String NULL_VALUE = "\\N";
     public static final int CACHE_BUFFER_SIZE = 256 * 1024;
     public static final int CACHE_BUFFER_COUNT = 3;
