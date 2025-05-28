e9397d12512ba7f2bdb6aa6e8e98d23c4da7d469
mnianqi <mnq0626@163.com>
2025-05-28 10:45:07 +0800

    fix: TAP-6786 validate doris be nodes when testing connection write privilege

--- connectors/doris-connector/src/main/java/io/tapdata/connector/doris/DorisTest.java
+++ connectors/doris-connector/src/main/java/io/tapdata/connector/doris/DorisTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 import static io.tapdata.base.ConnectorBase.testItem;
