9ea30ad3c6f2e442312615e05ce19b9f4aaa3740
sam.p <102567930@qq.com>
2025-06-18 19:56:25 +0800

    fix: TAP-6890 Master-slave merge task. After the primary key is changed, task error: When writing data, unique constraints are violated
    Closes TAP-6890

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/reader/MongodbV4StreamReader.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/reader/MongodbV4StreamReader.java
@@ -158,11 +158,6 @@ public class MongodbV4StreamReader implements MongodbStreamReader {
                     if (event == null) {
                         continue;
                     }
-                    RawBsonDocument fullDocument = event.getFullDocument();
-                    RawBsonDocument fullDocumentBeforeChange = event.getFullDocumentBeforeChange();
-                    if (null != fullDocumentBeforeChange && null != fullDocument && fullDocumentBeforeChange.get("CLAIM_ID").equals(fullDocument.get("CLAIM_ID"))) {
-                        System.out.println("ttt before = " + fullDocumentBeforeChange + ", after = " + fullDocument);
-                    }
                     concurrentProcessor.runAsync(event, e -> {
                         try {
                             return emit(e);
