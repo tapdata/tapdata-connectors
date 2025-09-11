79b1c23058904cab3b0664d0c9b9540ca279d5dd
jarad0628 <coolgj@163.com>
2025-09-12 00:30:51 +0800

    fix: connectionTest write and read failed with warning info

--- connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/PostgresTest.java
+++ connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/PostgresTest.java
@@ -4,19 +4,20 @@ import com.google.common.collect.Lists;
 import io.tapdata.common.CommonDbTest;
 import io.tapdata.connector.postgres.config.PostgresConfig;
 import io.tapdata.entity.simplify.TapSimplify;
-import io.tapdata.kit.EmptyKit;
 import io.tapdata.kit.StringKit;
 import io.tapdata.pdk.apis.entity.ConnectionOptions;
 import io.tapdata.pdk.apis.entity.TestItem;
 import io.tapdata.pdk.apis.exception.testItem.TapTestCurrentTimeConsistentEx;
 import io.tapdata.pdk.apis.exception.testItem.TapTestReadPrivilegeEx;
 import io.tapdata.pdk.apis.exception.testItem.TapTestStreamReadEx;
-import io.tapdata.pdk.apis.exception.testItem.TapTestWritePrivilegeEx;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
@@ -58,6 +59,12 @@ public class PostgresTest extends CommonDbTest {
     //Test number of tables and privileges
     public Boolean testReadPrivilege() {
         try {
+            AtomicBoolean isDBA = new AtomicBoolean();
+            jdbcContext.queryWithNext(PG_SUPER_USER, resultSet -> isDBA.set(resultSet.getBoolean(1)));
+            if (isDBA.get()) {
+                consumer.accept(testItem(TestItem.ITEM_READ, TestItem.RESULT_SUCCESSFULLY, "Current user has DBA privilege"));
+                return true;
+            }
             AtomicInteger tableSelectPrivileges = new AtomicInteger();
             jdbcContext.queryWithNext(String.format(PG_TABLE_SELECT_NUM, StringKit.escape(commonDbConfig.getUser(), "'"),
                     StringKit.escape(commonDbConfig.getDatabase(), "'"), StringKit.escape(commonDbConfig.getSchema(), "'")), resultSet -> tableSelectPrivileges.set(resultSet.getInt(1)));
@@ -120,38 +127,13 @@ public class PostgresTest extends CommonDbTest {
         return tableCount.get();
     }
 
+    private final static String PG_SUPER_USER = "select rolsuper from pg_roles where rolname=current_user";
     private final static String PG_TABLE_NUM = "SELECT COUNT(*) FROM pg_tables WHERE schemaname='%s'";
     private final static String PG_TABLE_SELECT_NUM = "SELECT count(*) FROM information_schema.table_privileges " +
             "WHERE grantee='%s' AND table_catalog='%s' AND table_schema='%s' AND privilege_type='SELECT'";
     protected final static String PG_LOG_PLUGIN_CREATE_TEST = "SELECT pg_create_logical_replication_slot('%s','%s')";
     protected final static String PG_LOG_PLUGIN_DROP_TEST = "select pg_drop_replication_slot(a.slot_name) " +
             "from (select * from pg_replication_slots where slot_name like 'test_tapdata_%') a;";
-
-    @Override
-    protected Boolean testWritePrivilege() {
-        try {
-            List<String> sqls = new ArrayList<>();
-            String schemaPrefix = EmptyKit.isNotEmpty(commonDbConfig.getSchema()) ? ("\"" + StringKit.escape(commonDbConfig.getSchema(), "\"") + "\".") : "";
-            if (jdbcContext.queryAllTables(Arrays.asList(TEST_WRITE_TABLE, TEST_WRITE_TABLE.toUpperCase())).size() > 0) {
-                sqls.add(String.format(TEST_DROP_TABLE, schemaPrefix + TEST_WRITE_TABLE));
-            }
-            //create
-            sqls.add(String.format(TEST_CREATE_TABLE, schemaPrefix + TEST_WRITE_TABLE));
-            //insert
-            sqls.add(String.format(TEST_WRITE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
-            //update
-            sqls.add(String.format(TEST_UPDATE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
-            //delete
-            sqls.add(String.format(TEST_DELETE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
-            //drop
-            sqls.add(String.format(TEST_DROP_TABLE, schemaPrefix + TEST_WRITE_TABLE));
-            jdbcContext.batchExecute(sqls);
-            consumer.accept(testItem(TestItem.ITEM_WRITE, TestItem.RESULT_SUCCESSFULLY, TEST_WRITE_SUCCESS));
-        } catch (Exception e) {
-            consumer.accept(new TestItem(TestItem.ITEM_WRITE, new TapTestWritePrivilegeEx(e), TestItem.RESULT_SUCCESSFULLY_WITH_WARN));
-        }
-        return true;
-    }
 
     @Override
     public Boolean testTimeDifference() {
