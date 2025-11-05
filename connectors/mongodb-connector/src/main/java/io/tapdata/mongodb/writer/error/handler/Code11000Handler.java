9dcd30df964cf60cf44914b972381a35461c2eff
sam.p <102567930@qq.com>
2025-11-05 17:12:30 +0800

    fix(mongodb): Increase fault tolerance when encountering 11000 (duplicate key) error when writing to mongodb

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/writer/error/handler/Code11000Handler.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/writer/error/handler/Code11000Handler.java
@@ -4,16 +4,12 @@ import com.mongodb.MongoBulkWriteException;
 import com.mongodb.bulk.BulkWriteError;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.BulkWriteOptions;
-import com.mongodb.client.model.InsertOneModel;
-import com.mongodb.client.model.UpdateManyModel;
 import com.mongodb.client.model.WriteModel;
-import io.tapdata.entity.logger.TapLogger;
+import io.tapdata.mongodb.util.MapUtil;
 import io.tapdata.mongodb.writer.BulkWriteModel;
 import io.tapdata.mongodb.writer.error.BulkWriteErrorHandler;
-import org.apache.commons.collections4.CollectionUtils;
+import io.tapdata.mongodb.writer.error.IgnoreWriteModel;
 import org.bson.Document;
-
-import java.util.List;
 
 /**
  * @author samuel
@@ -21,7 +17,6 @@ import java.util.List;
  * @create 2023-04-23 19:10
  **/
 public class Code11000Handler implements BulkWriteErrorHandler {
-	private static final String TAG = Code11000Handler.class.getSimpleName();
 
 	@Override
 	public WriteModel<Document> handle(
@@ -32,54 +27,37 @@ public class Code11000Handler implements BulkWriteErrorHandler {
 			BulkWriteError writeError,
 			MongoCollection<Document> collection
 	) {
-		if (bulkWriteModel.isAllInsert() || isContainDocument(bulkWriteModel, writeError)) {
-			int index = writeError.getIndex();
-			List<WriteModel<Document>> allOpWriteModels = bulkWriteModel.getAllOpWriteModels();
-			if (CollectionUtils.isEmpty(allOpWriteModels)) {
-				return null;
-			}
-			try {
-				return allOpWriteModels.get(index);
-			} catch (Exception ignored) {
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
-
-	private boolean isContainDocument(BulkWriteModel bulkWriteModel, BulkWriteError writeError) {
-		if (CollectionUtils.isEmpty(bulkWriteModel.getOnlyInsertWriteModels())) {
-			return false;
-		}
+		// When duplicate key error occurs, check by filter whether the target document already exists.
+		// If exists, mark current operation to be ignored (no retry, no error)
 		try {
-			WriteModel<Document> errorWriteModel;
-			int index = writeError.getIndex();
-			List<WriteModel<Document>> allOpWriteModels = bulkWriteModel.getAllOpWriteModels();
-			if (CollectionUtils.isEmpty(allOpWriteModels)) {
-				return false;
+			Document filter = updateModelFilter(writeModel);
+			Document update = updateModelUpdate(writeModel);
+			if (filter == null || filter.isEmpty() || update == null || update.isEmpty() || collection == null) {
+				return null;
 			}
-			try {
-				errorWriteModel = allOpWriteModels.get(index);
-				if (errorWriteModel == null) {
-					return false;
+			Object set = update.get("$set");
+			if (set instanceof Document) {
+				update = (Document) set;
+			}
+			if (update.isEmpty()) {
+				return null;
+			}
+			filter = new Document(filter);
+			for (String key : filter.keySet()) {
+				Object value = MapUtil.getValueByKey(update, key);
+				if (null == value) {
+					return null;
 				}
-			} catch (Exception ignored) {
-				return false;
+				filter.put(key, value);
 			}
-			for (WriteModel<Document> writeModel : bulkWriteModel.getOnlyInsertWriteModels()) {
-				if (errorWriteModel instanceof UpdateManyModel) {
-					String id = ((Document) ((UpdateManyModel) errorWriteModel).getFilter()).get("_id").toString();
-					String idTemp = ((Document) ((InsertOneModel) writeModel).getDocument()).get("_id").toString();
-					if (id.equals(idTemp)) {
-						return true;
-					}
-				}
+			Document existed = collection.find(filter).limit(1).first();
+			if (existed != null) {
+				// Mark as ignored
+				return IgnoreWriteModel.INSTANCE;
 			}
-		} catch (Exception e) {
-			TapLogger.error(TAG, "Code11000Handler handle containDocument error", e);
-			return false;
+		} catch (Throwable ignored) {
+			// fall through to return null (can't handle)
 		}
-		return false;
+		return null;
 	}
 }
