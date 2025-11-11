909670f3347abcf5351900bb23b33eefa1625109
jarad0628 <coolgj@163.com>
2025-11-11 14:06:47 +0800

    fix: sqlserver objToString bug
    (cherry picked from commit d85a5572f78151634a6d4f2842b1f4de40afa962)

--- connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalWriteRecorder.java
+++ connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalWriteRecorder.java
@@ -495,7 +495,7 @@ public abstract class NormalWriteRecorder {
         if (null == obj) {
             result = "null";
         } else if (obj instanceof String) {
-            result = "'" + ((String) obj).replace("\\", "\\\\").replace("'", "\\'").replace("(", "\\(").replace(")", "\\)") + "'";
+            result = transferString((String) obj);
         } else if (obj instanceof Number) {
             result = obj.toString();
         } else if (obj instanceof Date) {
@@ -514,5 +514,20 @@ public abstract class NormalWriteRecorder {
             return "'" + obj + "'";
         }
         return result;
+    }
+
+    protected String transferString(String str) {
+        return "'" + str.replace("\\", "\\\\").replace("'", "''") + "'";
+    }
+
+    protected String byteArrayToHexString(byte[] bytes) {
+        if (bytes == null || bytes.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02X", b));
+        }
+        return "0x"+ sb;
     }
 }
