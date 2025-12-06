ee1e2f90a9c339ef685508a8ac35efc0e7492284
jarad0628 <coolgj@163.com>
2025-12-07 02:34:59 +0800

    fix: sqlserver improve performance ignore autoInc

--- connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalRecordWriter.java
+++ connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalRecordWriter.java
@@ -221,6 +221,13 @@ public class NormalRecordWriter {
         this.autoIncFields = autoIncFields;
     }
 
+    public void setLargeSql(boolean largeSql) {
+        this.largeSql = largeSql;
+        insertRecorder.setLargeSql(largeSql);
+        updateRecorder.setLargeSql(largeSql);
+        deleteRecorder.setLargeSql(largeSql);
+    }
+
     public Map<String, Object> getAutoIncMap() {
         return autoIncMap;
     }
