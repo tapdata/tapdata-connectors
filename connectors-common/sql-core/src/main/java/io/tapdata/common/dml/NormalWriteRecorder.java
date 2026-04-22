bc2c0c40a3bf7a5f10c199fc752cc8bec81c954f
jarad0628 <coolgj@163.com>
2026-04-22 10:04:06 +0800

    fix: simpledateformat multi-thread bug

--- connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalWriteRecorder.java
+++ connectors-common/sql-core/src/main/java/io/tapdata/common/dml/NormalWriteRecorder.java
@@ -545,7 +545,10 @@ public abstract class NormalWriteRecorder {
         return value;
     }
 
-    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+    // SimpleDateFormat 非线程安全，用 ThreadLocal 保证每个线程独立持有一个实例
+    protected static final ThreadLocal<DateFormat> dateFormat =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+    // DateTimeFormatter 不可变，天然线程安全
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
 
     protected String object2String(Object obj) {
@@ -557,7 +560,7 @@ public abstract class NormalWriteRecorder {
         } else if (obj instanceof Number) {
             result = obj.toString();
         } else if (obj instanceof Date) {
-            result = "'" + dateFormat.format(obj) + "'";
+            result = "'" + dateFormat.get().format(obj) + "'";
         } else if (obj instanceof Instant) {
             result = "'" + LocalDateTime.ofInstant((Instant) obj, ZoneId.of("GMT")).format(dateTimeFormatter) + "'";
         } else if (obj instanceof byte[]) {
