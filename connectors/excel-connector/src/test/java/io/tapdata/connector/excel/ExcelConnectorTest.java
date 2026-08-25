e9c984bce4eeaf0987fa90a66bb84c51e8000bb7
GavinXiao <2749984520@qq.com>
2026-08-25 11:17:43 +0800

    fix(TAP-12724): Error parsing time of 0 seconds

--- connectors/excel-connector/src/test/java/io/tapdata/connector/excel/ExcelConnectorTest.java
+++ connectors/excel-connector/src/test/java/io/tapdata/connector/excel/ExcelConnectorTest.java
@@ -1,18 +1,25 @@
 package io.tapdata.connector.excel;
 
 import io.tapdata.connector.excel.util.CellValueConvert;
+import io.tapdata.entity.codec.FromTapValueCodec;
+import io.tapdata.entity.codec.TapCodecsRegistry;
+import io.tapdata.entity.codec.ToTapValueCodec;
 import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.schema.value.TapTimeValue;
+import io.tapdata.pdk.apis.functions.ConnectorFunctions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExcelConnectorTest {
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     @Test
     void makeTapTableMapsJavaTimeValuesToExcelDataTypes() {
@@ -44,5 +51,22 @@ class ExcelConnectorTest {
         ExcelConnector connector = new ExcelConnector();
 
         assertEquals("10.00", CellValueConvert.parseValue(10L, "10.00", "STRING"));
+    }
+
+    @Test
+    void registerCapabilitiesConvertsEverySecondLocalTimeToTapTime() {
+        ExcelConnector connector = new ExcelConnector();
+        TapCodecsRegistry codecRegistry = TapCodecsRegistry.create();
+        connector.registerCapabilities(new ConnectorFunctions(), codecRegistry);
+        ToTapValueCodec<?> toTapValueCodec = codecRegistry.getCustomToTapValueCodec(LocalTime.class);
+        FromTapValueCodec<TapTimeValue> fromTapValueCodec = codecRegistry.getFromTapValueCodec(TapTimeValue.class);
+
+        for (int secondOfDay = 0; secondOfDay < 24 * 60 * 60; secondOfDay++) {
+            LocalTime localTime = LocalTime.ofSecondOfDay(secondOfDay);
+            TapTimeValue tapTimeValue = (TapTimeValue) toTapValueCodec.toTapValue(localTime, null);
+            Object value = fromTapValueCodec.fromTapValue(tapTimeValue);
+
+            assertEquals(localTime.format(TIME_FORMATTER), value, "secondOfDay=" + secondOfDay);
+        }
     }
 }
